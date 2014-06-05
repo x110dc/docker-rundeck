0da1061654236fefc174055237ec8fff075f9219
Daniel Craigmile <dcraigmile@texastribune.org>
2014-06-05 18:30:19 -0500

    stop generating absolute URLs

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ sed -i 's/&>>\/var\/log\/rundeck\/service.log &$//g' /etc/init.d/rundeckd
 # Change the Rundeck admin password
 sed -i "s/^admin:admin/admin:$RDPASS/g" /etc/rundeck/realm.properties
 
-# Change MYHOST to your IP or hostname
-sed -i "s/localhost:4440/$MYHOST:4440/g" /etc/rundeck/rundeck-config.properties
+# remove the grails.serverURL so that absolute URLs aren't generated
++sed -i '/grails.serverURL/d' /etc/rundeck/rundeck-config.properties
 
 # Generate a new passwordless SSH key
 mkdir -p /var/lib/rundeck/.ssh/
